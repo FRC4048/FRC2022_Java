c070c917466992db1b6966aa5ad0766ba16c3fe1
XiaoleCc <charlottesu0223@gmail.com>
2022-01-29 22:18:38 -0500

    added the right motor util one

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,6 +5,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
@@ -21,6 +22,8 @@ public class RobotContainer {
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+
+  public final PowerDistribution m_PowerDistPanel = new PowerDistribution();
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
---
+++ src/main/java/frc/robot/utils/MotorUtils.java
@@ -0,0 +1,52 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.utils;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Timer;
+import frc.robot.Robot;
+import frc.robot.utils.logging.Logging;
+
+/**
+ * Add your docs here.
+ */
+public class MotorUtils {
+    public static final double DEFAULT_TIMEOUT = 0.15;
+    private double timeout;
+    private double time;
+    final int PDPChannel;
+    final double currentThreshold;
+
+    public MotorUtils(int PDPPort, double currentThreshold) {
+        this.timeout = DEFAULT_TIMEOUT;
+        this.PDPChannel = PDPPort;
+        this.currentThreshold = currentThreshold;
+        time = Timer.getFPGATimestamp();
+    }
+
+    public MotorUtils(int PDPPort, double currentThreshold, double timeout) {
+        this(PDPPort, currentThreshold);
+        this.timeout = timeout;
+    }
+
+    public boolean isStalled() {
+        final double currentValue = Robot.m_robotContainer.m_PowerDistPanel.getCurrent(PDPChannel);
+        final double now = Timer.getFPGATimestamp();
+
+        if (currentValue < currentThreshold) {
+            time = now;
+        } else {
+            DriverStation.reportError("Motor stall, PDP Channel=" + PDPChannel, false);
+            if (now - time > timeout) {
+                Logging.instance().traceMessage(Logging.MessageLevel.INFORMATION, "Motor stall, PDP channel =" + PDPChannel);
+                return true;
+            }
+        }
+        return false;
+    }
+}
