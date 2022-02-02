1b2936565e3a72c7dc0b3576a0cfd63a0e3840a0
Ben-Krugman <benjamin.krugman@gmail.com>
2022-02-01 19:58:10 -0500

    Drivetrain do be driving :) the front of the chassis is labeled so dont hit the back >:(

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,10 +5,11 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.DriveTrain;
+import frc.robot.commands.Drive;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -18,13 +19,19 @@ import edu.wpi.first.wpilibj2.command.Command;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  //private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+  private static Joystick joyLeft = new Joystick(0);
+  private static Joystick joyRight = new Joystick(1);
+
+  private final DriveTrain drivetrain = new DriveTrain();
+
+  private final Drive command = new Drive(drivetrain, () -> joyLeft.getY(), () -> joyRight.getY());
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the button bindings
+    drivetrain.setDefaultCommand(command);
     configureButtonBindings();
   }
 
@@ -43,6 +50,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
-    return m_autoCommand;
+    return command;
   }
 }
--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -1,18 +1,70 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.sensors.PigeonIMU;
+import com.ctre.phoenix.sensors.PigeonIMUConfiguration;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Robot;
 
 public class DriveTrain extends SubsystemBase {
-    public DriveTrain(){
+  private WPI_TalonSRX left1;
+  private WPI_TalonSRX right1;
+  private WPI_TalonSRX left2;
+  private WPI_TalonSRX right2;
+  private DifferentialDrive driveTrain;
+  private Encoder leftEncoder;
+  private Encoder rightEncoder;
+  // private AHRS navX;
+  private PigeonIMU gyro;
+  private Solenoid gearSolenoid;
 
-    }
-    @Override
-    public void periodic() {
-        // This method will be called once per scheduler run
-    }
+  /**
+   * Creates a new DriveTrain.
+   */
+  public DriveTrain() {
+    left1 = new WPI_TalonSRX(6);
+    left2 = new WPI_TalonSRX(3);
+    right1 = new WPI_TalonSRX(7);
+    right2 = new WPI_TalonSRX(8);
 
-    @Override
-    public void simulationPeriodic() {
-        // This method will be called once per scheduler run during simulation
+    // navX = new AHRS(I2C.Port.kMXP);
+    left2.set(ControlMode.Follower, 6);
+    right2.set(ControlMode.Follower, 7);
+    
+    // right1.setInverted(true);
+    // right2.setInverted(true);    
+    left1.setInverted(true);
+    left2.setInverted(true);
+
+    left1.setNeutralMode(NeutralMode.Brake);
+    left2.setNeutralMode(NeutralMode.Brake);
+    right1.setNeutralMode(NeutralMode.Brake);
+    right2.setNeutralMode(NeutralMode.Brake);
+  }
+
+  /**
+   * Drives the robot by a speed between -1 and +1
+   * 
+   * @param speedLeft
+   * @param speedRight
+   */
+  public void drive(double speedLeft, double speedRight, boolean isSquared){
+    if(isSquared) {
+      speedLeft = Math.signum(speedLeft) * Math.pow(speedLeft, 2);
+      speedRight = Math.signum(speedRight) * Math.pow(speedRight, 2);
     }
+    // driveTrain.tankDrive(speedLeft, speedRight);
+    //The joysticks are inverted so inverting this makes it drive correctly.
+    left1.set(ControlMode.PercentOutput, speedLeft);
+    right1.set(ControlMode.PercentOutput, speedRight);
+  }
 }
