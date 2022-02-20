73be9ea40fff0d375724038733e7d72af05798b0
fclegg <59775798+fclegg@users.noreply.github.com>
2022-02-20 14:28:51 -0500

    it works now 👍

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -16,6 +16,7 @@ import frc.robot.commands.intakecommands.DropBallCommand;
 import frc.robot.commands.intakecommands.IntakeBallCommand;
 import frc.robot.commands.intakecommands.RaiseIntakeCommand;
 import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.LimelightSubsystem;
 import frc.robot.commands.Drive;
 import frc.robot.commands.intakecommands.IntakeSequence;
 import frc.robot.commands.ShooterCommands.TogglePiston;
@@ -23,6 +24,7 @@ import frc.robot.commands.ShooterCommands.ToggleShooterMotor;
 import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.Shooter;
 import frc.robot.utils.SmartShuffleboard;
+import frc.robot.utils.limelight.LimeLightVision;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
@@ -43,14 +45,17 @@ public class RobotContainer {
   private XboxController xboxController = new XboxController(Constants.CONTROLLER_ID);
   private  JoystickButton buttonA = new JoystickButton(xboxController, Constants.XBOX_A_BUTTON);
 
+  private final LimelightSubsystem limeLightVision = new LimelightSubsystem();
+
   private final IntakeSubsystem intakeSubsystem = new IntakeSubsystem();
   private final DriveTrain driveTrain = new DriveTrain();
-  private final Shooter shooterSubsystem = new Shooter();
+  //private final Shooter shooterSubsystem = new Shooter();
   private final PowerDistribution m_PowerDistPanel = new PowerDistribution();
 
   public AutoChooser autoChooser = new AutoChooser();
 
   private final Drive driveCommand = new Drive(driveTrain, () -> joyLeft.getY(), () -> joyRight.getY());
+
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -93,6 +98,7 @@ public class RobotContainer {
     return driveCommand;
   }
 
+
   public void installCommandsOnShuffleboard() {
     if (Constants.ENABLE_DEBUG) {
       SmartShuffleboard.putCommand("Intake", "Deploy Intake", new DeployIntakeCommand(getIntakeSubsystem()));
@@ -100,8 +106,11 @@ public class RobotContainer {
       SmartShuffleboard.putCommand("Intake", "Intake Ball", new IntakeBallCommand(getIntakeSubsystem()));
       SmartShuffleboard.putCommand("Intake", "Drop Ball", new DropBallCommand(getIntakeSubsystem()));
 
-      SmartShuffleboard.putCommand("Shooter", "Toggle Piston", new TogglePiston(shooterSubsystem));
-      SmartShuffleboard.putCommand("Shooter", "Toggle Shooter Motor", new ToggleShooterMotor(shooterSubsystem));
+
+      //SmartShuffleboard.putCommand("Shooter", "Toggle Piston", new TogglePiston(shooterSubsystem));
+      //SmartShuffleboard.putCommand("Shooter", "Toggle Shooter Motor", new ToggleShooterMotor(shooterSubsystem));
+      
+      
     }
   }
 } 
---
+++ src/main/java/frc/robot/subsystems/LimelightSubsystem.java
@@ -0,0 +1,27 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.utils.SmartShuffleboard;
+import frc.robot.utils.limelight.LimeLightVision;
+
+public class LimelightSubsystem extends SubsystemBase {
+  /** Creates a new LimelightSubsystem. */
+  
+  LimeLightVision vision;
+  public LimelightSubsystem() {
+    vision = new LimeLightVision(38, 104, 30);
+  }
+
+  @Override
+  public void periodic() {
+    if (vision.getCameraAngles() != null) {
+      SmartShuffleboard.put("Limelight", "Y Offset", vision.getCameraAngles().getTy());
+      SmartShuffleboard.put("Limelight", "Camera Distance", vision.getHorizontalDistanceToTarget(vision.getCameraAngles().getTy()));
+    }
+    // This method will be called once per scheduler run
+  }
+} 
