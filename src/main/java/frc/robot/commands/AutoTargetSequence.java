56b525806f5a689b153a61ef1fd9f94a37e862bf
Ben Gold <bengold@gmail.com>
2022-02-26 12:00:54 -0500

    Fixing compile errors

--- src/main/java/frc/robot/commands/AutoTargetSequence.java
+++ src/main/java/frc/robot/commands/AutoTargetSequence.java
@@ -4,8 +4,6 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.Miscellaneous.SetLEDOn;
 import frc.robot.subsystems.Hood;
--- src/main/java/frc/robot/commands/ShootSequence.java
+++ src/main/java/frc/robot/commands/ShootSequence.java
@@ -4,16 +4,11 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants;
-import frc.robot.commands.ShooterCommands.ExtendShooterPiston;
-import frc.robot.commands.ShooterCommands.RetractShooterPiston;
 import frc.robot.commands.ShooterCommands.RotateShooterMotor;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.Shooter;
-import frc.robot.utils.SmartShuffleboard;
 
 public class ShootSequence extends ParallelCommandGroup {
   /** Creates a new PistonSequence. */
