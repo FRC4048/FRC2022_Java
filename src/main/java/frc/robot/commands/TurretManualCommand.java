1c0acf5a798519eb633e0eb3651a081a2d5b507c
BenK <timofey.tylik@gmail.com>
2022-03-04 20:16:07 -0500

    mmmmmmmmmm loggers

--- src/main/java/frc/robot/commands/TurretManualCommand.java
+++ src/main/java/frc/robot/commands/TurretManualCommand.java
@@ -7,36 +7,34 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
 import frc.robot.subsystems.TurretSubsystem;
 
-public class TurretManualCommand extends CommandBase {
+public class TurretManualCommand extends LoggedCommand {
     private TurretSubsystem turretSubsystem;        
     private DoubleSupplier joystickInput;
 
     public TurretManualCommand(TurretSubsystem turretSubsystem, DoubleSupplier joystickXAxis) {
-        joystickInput=joystickXAxis;
+        joystickInput = joystickXAxis;
         addRequirements(turretSubsystem);
         this.turretSubsystem = turretSubsystem;
       
     }
     @Override
-    public void initialize() {
+    public void loggedInitialize() {
     }
 
     @Override
-    public void execute() {
+    public void loggedExecute() {
         //dont know if the input should be negative
       turretSubsystem.setTurret((joystickInput.getAsDouble() * Constants.TURRETSPIN_SCALEFACTOR));
     }
 
     @Override
-    public void end(boolean interrupted) {
+    public void loggedEnd(boolean interrupted) {
         turretSubsystem.stopTurret();
     }
 
     @Override
-    public boolean isFinished() {
+    public boolean loggedIsFinished() {
         return false;
 
     }
-
-
-  }
+}
