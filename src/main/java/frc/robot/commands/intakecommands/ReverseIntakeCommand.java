099f89d61bba38e7cdddfa9fb02bd63fab5dac16
Nathan <Nathan0108>
2022-03-12 14:31:35 -0500

    intake changes

--- src/main/java/frc/robot/commands/intakecommands/ReverseIntakeCommand.java
+++ src/main/java/frc/robot/commands/intakecommands/ReverseIntakeCommand.java
@@ -1,3 +1,7 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot.commands.intakecommands;
 
 import edu.wpi.first.wpilibj.Timer;
@@ -5,19 +9,14 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
 import frc.robot.subsystems.IntakeSubsystem;
 
-public class DropBallCommand extends CommandBase {
+public class ReverseIntakeCommand extends CommandBase {
+  /** Creates a new ReverseIntakeCommand. */
   private IntakeSubsystem intakeSubsystem;
   private double initTime;
-
-  /**
-   * Creates a new ExampleCommand.
-   *
-   * @param subsystem The subsystem used by this command.
-   */
-  public DropBallCommand(IntakeSubsystem intakeSubsystem) {
+  public ReverseIntakeCommand(IntakeSubsystem intakeSubsystem) {
     // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(intakeSubsystem);
     this.intakeSubsystem = intakeSubsystem;
+    addRequirements(intakeSubsystem);
   }
 
   // Called when the command is initially scheduled.
@@ -29,7 +28,7 @@ public class DropBallCommand extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    intakeSubsystem.spinMotor(Constants.INTAKE_MOTOR_SPEED);
+    intakeSubsystem.spinMotor(-Constants.INTAKE_MOTOR_SPEED);
   }
 
   // Called once the command ends or is interrupted.
@@ -41,7 +40,6 @@ public class DropBallCommand extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return (!intakeSubsystem.isBallInIntake() || (Timer.getFPGATimestamp() - initTime) >= Constants.RAISED_INTAKE_TIMEOUT);
-
+    return (!intakeSubsystem.isBallInIntake() || (Timer.getFPGATimestamp() - initTime) >= Constants.DEPLOYED_INTAKE_TIMEOUT);
   }
 }
