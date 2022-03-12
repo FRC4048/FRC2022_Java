ceb31a9d9e604a0d1d9b4d7d40e1c4ffdfc26134
Nathan <Nathan0108>
2022-03-12 14:46:45 -0500

    Formatting folders

--- src/main/java/frc/robot/commands/ShooterCommands/AutoTargetSequence.java
+++ src/main/java/frc/robot/commands/ShooterCommands/AutoTargetSequence.java
@@ -2,10 +2,12 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands;
+package frc.robot.commands.ShooterCommands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.HoodCommands.HoodAutoCommand;
 import frc.robot.commands.Miscellaneous.SetLEDOn;
+import frc.robot.commands.TurretCommands.TurretAuto;
 import frc.robot.subsystems.Hood;
 import frc.robot.subsystems.TurretSubsystem;
 import frc.robot.utils.SmartShuffleboard;
