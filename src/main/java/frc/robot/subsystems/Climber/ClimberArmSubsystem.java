7e41c0dd8249246f42ce9e461e8c06c843da1026
Nathan <Nathan0108>
2022-03-21 19:31:59 -0400

    y

--- src/main/java/frc/robot/subsystems/Climber/ClimberArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/Climber/ClimberArmSubsystem.java
@@ -13,7 +13,6 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.wpilibj.Solenoid;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.Robot;
