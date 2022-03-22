6219656a32d30e878e3a820330596e4199a7a691
Nathan <Nathan0108>
2022-03-22 19:05:28 -0400

    j

--- src/main/java/frc/robot/subsystems/Climber/ClimberWinchSubsystem.java
+++ src/main/java/frc/robot/subsystems/Climber/ClimberWinchSubsystem.java
@@ -11,7 +11,6 @@ import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.DigitalOutput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.Robot;
