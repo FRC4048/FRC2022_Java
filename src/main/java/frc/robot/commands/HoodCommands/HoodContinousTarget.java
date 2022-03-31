a7de4a387f12f43208595670c18862c1f3fb5019
QuokkaLord <timofey.tylik@gmail.com>
2022-03-30 20:38:24 -0400

    interrupt

--- src/main/java/frc/robot/commands/HoodCommands/HoodContinousTarget.java
+++ src/main/java/frc/robot/commands/HoodCommands/HoodContinousTarget.java
@@ -75,7 +75,9 @@ public class HoodContinousTarget extends CommandBase {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    hood.setHood(0);
+  }
 
   // Returns true when the command should end.
   @Override
