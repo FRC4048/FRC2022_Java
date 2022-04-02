c79f2659db5e84365d509957e1e4e06080573ae1
Bat-Chen <bzeliger@gmail.com>
2022-04-02 16:48:15 -0400

    debug stuff

--- src/main/java/frc/robot/commands/DriveCommands/AutoTurnDegrees.java
+++ src/main/java/frc/robot/commands/DriveCommands/AutoTurnDegrees.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
 import frc.robot.subsystems.DriveTrain;
+import frc.robot.utils.SmartShuffleboard;
 
 public class AutoTurnDegrees extends CommandBase{
 
@@ -30,13 +31,15 @@ public class AutoTurnDegrees extends CommandBase{
         double rawError = (startDegrees + turnDegrees - driveTrain.getAngle());
         double error = Math.abs(rawError);
         double direction = Math.signum(rawError);
+        SmartShuffleboard.put("Drive", "Turn error", error);
         if (error > Constants.AUTO_MOVE_TURN_SLOWDOWN_ERROR) {
             turnSpeed = Constants.AUTO_MOVE_TURN_MAX_SPEED;
         }
         else {
             turnSpeed = error/Constants.AUTO_MOVE_TURN_SLOWDOWN_ERROR * (Constants.AUTO_MOVE_TURN_MAX_SPEED - Constants.AUTO_MOVE_TURN_MIN_SPEED) + Constants.AUTO_MOVE_TURN_MIN_SPEED;
         }
-        driveTrain.drive(direction * turnSpeed, -1.0 * direction * turnSpeed, false);
+        SmartShuffleboard.put("Drive", "Turn speed", turnSpeed);
+        driveTrain.drive(-1.0 * direction * turnSpeed, direction * turnSpeed, false);
     }
 
     @Override
@@ -50,6 +53,7 @@ public class AutoTurnDegrees extends CommandBase{
             return true;
         }
         double error = Math.abs(startDegrees + turnDegrees - driveTrain.getAngle());
+        SmartShuffleboard.put("Drive", "Turn error", error);
         return (error <= Constants.AUTO_MOVE_TURN_THRESHOLD);
     }
     
