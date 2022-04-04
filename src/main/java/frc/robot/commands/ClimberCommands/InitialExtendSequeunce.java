1bb52cf37c1248e0c34c8c12fbc3591cac322d50
Nathan0108 <nathanmiller0108@gmail.com>
2022-04-04 18:49:56 -0400

    h

--- src/main/java/frc/robot/commands/ClimberCommands/InitialExtendSequeunce.java
+++ src/main/java/frc/robot/commands/ClimberCommands/InitialExtendSequeunce.java
@@ -4,7 +4,6 @@
 
 package frc.robot.commands.ClimberCommands;
 
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.ClimberCommands.AutoMoveClimberArm.ClimberDirection;
 import frc.robot.subsystems.TurretSubsystem;
