3fd98cd82ba22df317039015aa0bf94e6fe10319
Neo-Aditya <Hebbaradi25@student.westboroughk12.org>
2022-04-04 19:50:59 -0400

    y

---
+++ src/main/java/frc/robot/subsystems/IMUSubsystem.java
@@ -0,0 +1,46 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.sensors.PigeonIMU;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.ADIS16470_IMU;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.utils.SmartShuffleboard;
+
+
+public class IMUSubsystem extends SubsystemBase {
+  /** Creates a new IMUSubststem. */
+  private PigeonIMU gyro;
+
+  public IMUSubsystem() {
+
+  }
+
+  public void resetGyro() {
+    gyro.setFusedHeading(0);
+  }
+
+  public double getAngle() {
+    return Math.IEEEremainder(gyro.getFusedHeading(), 360);
+  }
+
+  public double getXaccel() {
+    return gyro.getAccelY();
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
