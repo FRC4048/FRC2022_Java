0dbf98ea9c727a72a2d7a0b86fd7216413bd9252
Finn Clegg <lagmacubems@gmail.com>
2022-05-24 18:06:33 -0400

    h

--- src/main/java/frc/robot/CopyCommand.java
+++ src/main/java/frc/robot/CopyCommand.java
@@ -6,8 +6,8 @@ import frc.robot.utils.SmartShuffleboard;
 public class CopyCommand extends CommandBase {
     @Override
     public void execute() {
-        double in = SmartShuffleboard.getDouble("test", "in", 0.0);
-        SmartShuffleboard.put("test", "out", in);
+        double in = SmartShuffleboard.getDouble("test", "Input", 0.0);
+        SmartShuffleboard.put("test", "Output", in);
     }
 
     @Override
